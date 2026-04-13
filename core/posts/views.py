8f383c3e84aeaa00250dcc2193711b633a8bd6b8
Abduazim <abdumajidovabduazim@gmail.com>
2026-04-13 15:34:52 +0500

    4-normativ: Search va Pagination qoshildi

--- core/posts/views.py
+++ core/posts/views.py
@@ -1,3 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    q = request.GET.get('q', '')
+    posts = Post.objects.all()
+
+    if q:
+        posts = posts.filter(title__icontains=q) | posts.filter(content__icontains=q)
+
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'posts/list.html', {
+        'page_obj': page_obj,
+        'q': q,
+    })
